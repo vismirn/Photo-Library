e9e0918af139c0f7a596340ae50566b8a6326b9a
Viktor <smirn04@yandex.ru>
2020-05-30 10:55:44 +0300

    Initial Commit

---
+++ Photo Library/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Library
+//
+//  Created by Viktor Smirnov on 30.05.2020.
+//  Copyright © 2020 Viktor Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
