89c1804a7123f60e04775d28380a43d646674fcf
Viktor <smirn04@yandex.ru>
2020-06-02 22:38:05 +0300

    использовал метод для реализации ДЗЖ Ширина ячейки и расположение как на картинке.. но у меня ничего не получилось...на изучение большего колва информации у меня не достаточно времени...и поэтому опять не смог доделать домашку..

---
+++ Photo Library/For Cell Constraint.swift
@@ -0,0 +1,15 @@
+//
+//  For Cell Constraint.swift
+//  Photo Library
+//
+//  Created by Viktor Smirnov on 02.06.2020.
+//  Copyright © 2020 Viktor Smirnov. All rights reserved.
+//
+
+import Foundation
+struct MyCollectionViewConstants{
+    static let CELL_ANY_ANY_REUSE_ID = "cell";
+    static let CELL_COMPACT_REGULAR_REUSE_ID = "cellSmall"
+    static let CELL_ANY_ANY_SIZE = 100;
+    static let CELL_COMPACT_REGULAR_SIZE = 70;
+}
--- Photo Library/ViewController.swift
+++ Photo Library/ViewController.swift
@@ -8,13 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    var cellSize = MyCollectionViewConstants.CELL_COMPACT_REGULAR_SIZE
+    var cellReuseID = MyCollectionViewConstants.CELL_ANY_ANY_REUSE_ID
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        setupNavigationController()
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
     }
+    
+    
+    
+    //приставка objc ставиться только если функция идёт в #selector (метод, выше)
+    @objc func addButtonPressed() {
+    }
+    
+    @objc func sharedButtonPressed() {
+    }
+    
+    
+    func setupNavigationController() {
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
+        let sharedButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(sharedButtonPressed))
+        navigationItem.rightBarButtonItems = [sharedButton, addButton]
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "PHOTOS"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = .darkGray
+        let photoTitle = UIBarButtonItem.init(customView: titleLabel)
+        navigationItem.leftBarButtonItem = photoTitle
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+    
+        return cell
+        
+    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
 
+        if (self.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact
+            && self.traitCollection.verticalSizeClass == UIUserInterfaceSizeClass.regular){
+            cellSize = MyCollectionViewConstants.CELL_COMPACT_REGULAR_SIZE
+            cellReuseID = MyCollectionViewConstants.CELL_COMPACT_REGULAR_REUSE_ID
+        } else {
+            cellSize = MyCollectionViewConstants.CELL_ANY_ANY_SIZE
+            cellReuseID = MyCollectionViewConstants.CELL_ANY_ANY_REUSE_ID
+        }
 
+        self.collectionView.reloadItems(
+            at: self.collectionView.indexPathsForVisibleItems)
+    }
+     
+//    override func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+//        return col
+//    }
+    
+    
+    private func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForCellAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    return CGSize(width: collectionView.frame.width/44, height: collectionView.frame.height)
+    }
+    
+    
+    
 }
+
+
 
